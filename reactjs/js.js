d638139290940f4a4c3b969c095529c7d6634a74
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2022-10-22 10:56:03 +0700

    update

---
+++ reactjs/js.js
@@ -0,0 +1,28 @@
+function isNum(str) {
+  switch (str) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9": {
+      return true;
+    }
+    default: {
+      return false;
+    }
+  }
+}
+
+let name = "Kienroro2003";
+let count = 0;
+for (let i = 0; i < name.length; i++){
+    if (isNum(name.charAt(i))) {
+        count++;
+    }
+}
+console.log(count);
